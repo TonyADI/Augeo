cd3d63df717b4551273df15c515bbbe1e9e32fb7
TonyADI <35707107+TonyADI@users.noreply.github.com>
2022-01-24 15:32:16 -0500

    change footer color and size

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 export const Footer = props => {
     return (
         <footer id="footer" className="full-width">
-          © Copyright TonyADI. All Rights Reserved.
+          &#169; Copyright TonyADI. All Rights Reserved.
           v1.0
         </footer>
     )
