f9509f4f9bcfd4cf7ba9f86b1041177a25880a79
TonyADI <35707107+TonyADI@users.noreply.github.com>
2022-06-16 21:03:35 -0400

    add server url and remove dummy products

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,7 +5,7 @@ import { retrieveData } from '../../utilities/projectAPI';
 import { AuthenticatedContext, AlertContext } from '../../components/App/App';
 import './Home.css';
 
-export const Home = props => {
+export const Home = () => {
     const [featured, setFeatured] = useState([]);
     const [latest, setLatest] = useState([]);
     const [popular, setPopular] = useState([]);
@@ -19,71 +19,39 @@ export const Home = props => {
     }
     
     const retrieveFeaturedProduct = () => {
-        retrieveData(`https://tonyadi.loca.lt/products?sortBy=featured`)
+        retrieveData(`https://augeo-server.herokuapp.com/products?sortBy=featured`)
         .then(data => {
             setFeatured(data);
         })
     }
 
     const retrieveLatestCategory = () => {
-        retrieveData(`https://tonyadi.loca.lt/products?sortBy=latest`)
+        retrieveData(`https://augeo-server.herokuapp.com/products?sortBy=latest`)
         .then(data => {
             setLatest(data);
         })
     }
 
     const retrievePopularProduct = () => {
-        retrieveData(`https://tonyadi.loca.lt/products?sortBy=popular`)
+        retrieveData(`https://augeo-server.herokuapp.com/products?sortBy=popular`)
         .then(data => {
             setPopular(data);
         })
     }
 
     const retrieveTrendingProduct = () => {
-        retrieveData(`https://tonyadi.loca.lt/products?sortBy=trending`)
+        retrieveData(`https://augeo-server.herokuapp.com/products?sortBy=trending`)
         .then(data => {
             setTrending(data);
         })
     }
 
     const retrieveRecentBids = () => {
-        retrieveData(`https://tonyadi.loca.lt/products?sortBy=recent`)
+        retrieveData(`https://augeo-server.herokuapp.com/products?sortBy=recent`)
         .then(data => {
             setRecentBids(data);
         })
     }
-    
-
-    const countDownDate = new Date("Jan 24, 2022 16:55:25").getTime();
-    const countDownDate2 = new Date("Feb 22, 2022 15:58:25").getTime();
-    const countDownDate3 = new Date("Mar 29, 2022 20:59:25").getTime();
-
-    // Dummy products to test functionality
-    const product = { category_name: 'HP Spectre x360', 
-                      initial_price: 1840, 
-                      current_ask: 0, 
-                      buy_now: 2500, 
-                      duration: countDownDate,
-                      id: 1
-                    };
-                    
-    const product2 = { category_name: 'iPhone 11', 
-                       initial_price: 670, 
-                       current_ask:845, 
-                       buy_now: 1600, 
-                       duration: countDownDate2,
-                       id: 2
-                    };
-
-    const product3 = { category_name: 'G-Shock', 
-                       initial_price: 70, 
-                       current_ask:0, 
-                       buy_now: 200, 
-                       duration: countDownDate3,
-                       id: 3
-                     };
-
-    const dummyProducts =  [product, product2, product3];
 
     useEffect(()=> {
         retrieveFeaturedProduct();
@@ -123,7 +91,6 @@ export const Home = props => {
                 </div>
             </div>
 
-            <ProductList heading='Dummy Products' products={dummyProducts}/>
             <ProductList heading='Featured Product' products={featured}/>
             <ProductList heading='Most Popular' products={popular}/>
             <ProductList heading='Trending' products={trending}/>
