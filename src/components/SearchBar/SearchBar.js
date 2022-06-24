df8d40614d90214fce30ba5598c6f42af2fda7f6
TonyADI <35707107+TonyADI@users.noreply.github.com>
2022-06-23 21:29:21 -0400

    Update SearchBar.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,34 +1,30 @@
 import React, { useState } from 'react';
 import './SearchBar.css';
 
-export const SearchBar = () => {
-    const [term, setTerm] = useState('');
+export const SearchBar = ({handleSubmit}) => {
+    const [searchTerm, setSearchTerm] = useState('');
     const [focus, setFocus] = useState(false);
 
     const handleChange = e => {
-        setTerm(e.target.value)
+        setSearchTerm(e.target.value)
     }
-
-   const handleKeyPress = e => {
-   }
     
     return (
-        <div 
-            id="search-container" 
-            style={{boxShadow: focus ? 
+        <div id="search-container" 
+             style={{boxShadow: focus ? 
                         '0px 0px 1px 1px #007bff': 
                         '0px 0px 1px 1px #e5e5e5'
                   }}>
-            <i className="fa fa-search search-icon"></i>
-            <input 
-                id="search-field" 
-                value={term} 
-                placeholder="Search" 
-                onChange={handleChange} 
-                onKeyPress={handleKeyPress}
-                onFocus={() => setFocus(true)}
-                onBlur={() => setFocus(false)}
-            />
+            <form onSubmit={e => handleSubmit(e, searchTerm)}>
+                <i className="fa fa-search search-icon"/>
+                    <input 
+                        id="search-field" 
+                        value={searchTerm} 
+                        placeholder="Search" 
+                        onChange={handleChange}
+                        onFocus={() => setFocus(true)}
+                        onBlur={() => setFocus(false)}/>
+            </form>
         </div>
     )
 }
